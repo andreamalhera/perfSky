e287d3801dc88133a3f8426185abbb1296e39d8c
Andrea Maldonado <andream@dev-box.trustyou.com>
2019-10-23 11:38:25 +0200

    Sepparate pm4py example from run

---
+++ processmining/pm4pyExample.py
@@ -0,0 +1,20 @@
+from pm4py.algo.discovery.alpha import factory as alpha_miner
+from pm4py.objects.log.importer.xes import factory as xes_importer
+from pm4py.visualization.petrinet import factory as vis_factory
+
+
+def run_example(log_path, output_path):
+    log = xes_importer.import_log(log_path)
+    net, initial_marking, final_marking = alpha_miner.apply(log)
+
+    for case_index, case in enumerate(log):
+        print("\n case index: %d  case id: %s" % (case_index,
+              case.attributes["concept:name"]))
+    for event_index, event in enumerate(case):
+        print("event index: %d  event activity: %s" % (event_index,
+              event["concept:name"]))
+
+    gviz = vis_factory.apply(net, initial_marking, final_marking)
+    gviz.graph_attr['bgcolor'] = 'white'
+    vis_factory.save(gviz, output_path)
+    # vis_factory.view(gviz)
--- processmining/run.py
+++ processmining/run.py
@@ -1,7 +1,4 @@
-from pm4py.algo.discovery.alpha import factory as alpha_miner
-from pm4py.objects.log.importer.xes import factory as xes_importer
-from pm4py.visualization.petrinet import factory as vis_factory
-
+from pm4pyExample import run_example
 # TODO: Read and extract interesting information from Luigi Logs
 # TODO: Save that information as csv or xes
 # TODO: Use csv and xes to visualize process
@@ -9,18 +6,4 @@ from pm4py.visualization.petrinet import factory as vis_factory
 LOG_PATH = './data/dockervolume/running-example-just-two-cases.xes'
 OUTPUT_PATH = './data/dockervolume/example.png'
 
-parameters = {"timestamp_sort": True}
-log = xes_importer.import_log(LOG_PATH)
-net, initial_marking, final_marking = alpha_miner.apply(log)
-
-for case_index, case in enumerate(log):
-    print("\n case index: %d  case id: %s" % (case_index,
-          case.attributes["concept:name"]))
-    for event_index, event in enumerate(case):
-        print("event index: %d  event activity: %s" % (event_index,
-              event["concept:name"]))
-
-gviz = vis_factory.apply(net, initial_marking, final_marking)
-gviz.graph_attr['bgcolor'] = 'white'
-vis_factory.save(gviz, OUTPUT_PATH)
-# vis_factory.view(gviz)
+run_example(LOG_PATH, OUTPUT_PATH)
