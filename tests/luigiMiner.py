e820f3b50b17519980d999375fb48cc3905fe786
Andrea Maldonado <andream@dev-box.trustyou.com>
2019-11-05 14:34:21 +0100

    Fixes lint and data path for tests

--- tests/luigiMiner.py
+++ tests/luigiMiner.py
@@ -1,6 +1,6 @@
 from processmining.luigiMiner import data_from_log, file_to_df
 
-LUIGI_LOG_PATH = './data/minilogs/head_daily.2019-09-01_09-29-01.log'
+LUIGI_LOG_PATH = './data/minilogs/daily.2019-09-01_09-29-01.log'
 
 
 def test_file_to_df():
@@ -11,6 +11,6 @@ def test_file_to_df():
 
 def test_data_from_log():
     df = data_from_log(LUIGI_LOG_PATH)
-    assert set(['timestamp', 'number', 'mode', 'line_in_code', 'message'])\
-            .issubset(df.columns)
+    assert set(['timestamp', 'number', 'mode',
+                'line_in_code', 'message']).issubset(df.columns)
     assert df['timestamp'][0] == '2019-09-01 09:29:23'
