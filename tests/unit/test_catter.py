f9b12f05f747297c97b8ae736997e1cdef7a3705
Andrea Maldonado <andream@dev-box.trustyou.com>
2020-02-04 13:34:54 +0100

    Move get_task from notebook

---
+++ tests/unit/test_catter.py
@@ -0,0 +1,9 @@
+from processmining.preprocessing.catter import get_task
+
+
+def test_get_task():
+    expected_task = 'CrawlTask'
+
+    assert get_task('CrawlTask(date=2019-09-04_23-23-01, prev_date=2019-09-03_03-44-01)\n') == expected_task
+    assert get_task('') == ''
+    assert get_task('CrawlTask') == expected_task
