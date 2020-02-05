fa374e39460d9772d2764e707e58571eb8f91d54
Andrea Maldonado <andream@dev-box.trustyou.com>
2020-02-05 14:55:10 +0100

    Sort todos to corresponding modules

--- processmining/run.py
+++ processmining/run.py
@@ -3,13 +3,19 @@ import os
 from pm4pyExample import run_inductiveminer_example as pm4py_example
 from preprocessing.luigi_miner import run_luigi_log_miner as luigi_miner
 
+# TODO: Visualize process as in boxplot notebook
+# TODO: Expand parameters for every task_call and clean those parameters
 
 start = datetime.datetime.now()
 
 TOY_CSV_PATH = './data/pm4pyexample/running-example-just-two-cases.csv'
 TOY_XES_PATH = './data/pm4pyexample/running-example-just-two-cases.xes'
-LUIGI_LOG_PATH = './data/minilogs/daily/logs'
-OUTPUT_PATH = './data/minilogs/daily/graphs'
+
+# TODO: Make directory of logs a dynamic argument from make for different pipelines
+PROCESS_NAME='table-precomp' # As in directory where logs are stored
+LUIGI_LOG_PATH = './data/minilogs/'+PROCESS_NAME+'/logs/october/mongo'
+OUTPUT_PATH = './data/minilogs/'+PROCESS_NAME+'/graphs'
+
 
 # pm4py_example(TOY_XES_PATH, OUTPUT_PATH+'/just_two_cases')
 files = [file for file in os.listdir(LUIGI_LOG_PATH) if file.endswith('.log')]
