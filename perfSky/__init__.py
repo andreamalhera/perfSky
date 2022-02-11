49feaebd53edf1e42fa25d31bf48ba1ee771fffe
Andrea Maldonado <>
2022-02-11 16:07:39 +0100

    add credits

--- perfSky/__init__.py
+++ perfSky/__init__.py
@@ -6,3 +6,4 @@ Performance Skyline python package to visualize event logs (from process mining)
 
 __version__ = "0.1.0"
 __author__ = 'Andrea Maldonado'
+__credits__='Ludwig Maximilian University Munich'
