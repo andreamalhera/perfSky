9cc3b268d2633a5e1cfcff5d676f65ffd2db0c96
Andrea Maldonado <>
2022-02-11 15:46:15 +0100

    Update package info in init

---
+++ perfSky/__init__.py
@@ -0,0 +1,8 @@
+"""
+perfSky.
+
+Performance Skyline python package to visualize event logs (from process mining) performance'Performance Skyline python package to visualize event logs (from process mining) performance.'
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Andrea Maldonado'
