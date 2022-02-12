71c190a79787e36e08911f4678902c4ddad26162
Andrea Maldonado <>
2022-02-12 07:35:31 +0100

    Adds flake8 for style guide enforcement

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
         url='https://github.com/andreamalhera/performanceskyline.git',
         install_requires=[
             'pytest',
-            'py4pm'
+            'py4pm',
+            'flake8'    #Tool for coding style guide enforcement
             ],
         classifiers=[
             'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
