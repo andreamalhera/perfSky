35af0344e5626b768a6126d8614492016ace1b20
Andrea Maldonado <>
2022-09-06 18:20:38 +0200

    Adds todos for anomaly detection

---
+++ perfSky/AnomalyDetection.py
@@ -0,0 +1,14 @@
+def get_ne_anomalies():
+    return
+
+def get_pes_anomalies():
+    return
+
+def get_sas_anomalies():
+    return
+
+def get_nepal_anomalies():
+    return
+
+def get_nerds_anomalies():
+    return
